f2d2e9f504794ca15c26a9bbfd3f1a06c27ee85e
rawane <96692209+RawaneG@users.noreply.github.com>
2023-06-04 00:02:13 +0000

    new commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@ import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ 
-  Component, 
+export default function App({
+  Component,
   pageProps: {
     session,
     ...pageProps
@@ -12,6 +13,9 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Netflix</title>
+      </Head>
         <Component {...pageProps} />
     </SessionProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { NextPageContext } from 'next';
-import { getSession } from 'next-auth/react';
-
 import Navbar from '@/components/Navbar';
+import { getSession } from 'next-auth/react';
 import Billboard from '@/components/Billboard';
 import MovieList from '@/components/MovieList';
 import InfoModal from '@/components/InfoModal';
